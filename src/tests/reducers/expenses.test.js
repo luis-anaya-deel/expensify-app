c9bafe5ee6c5077db6022392d5e777cacf24162f
Luis Anaya <lanaya@innomius.com>
2018-03-26 09:14:59 -0600

    Expensify initial commit

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,72 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, {type: '@@INIT'});
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expenses if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-1'
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test('should add an expense', () => {
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: '3',
+      description: 'Water',
+      note: '',
+      amount: 280,
+      createdAt: 2000
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, action.expense]);
+});
+
+test('should edit an expense', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[0].id,
+    updates: {
+      description: 'Gums',
+      amount: 390
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([{
+    id: '1',
+    description: 'Gums',
+    note: '',
+    amount: 390,
+    createdAt: 0
+  }, expenses[1], expenses[2]]);
+});
+
+test('should not edit expense if expense not found', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '-1',
+    updates: {
+      description: 'Gums',
+      amount: 390
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
